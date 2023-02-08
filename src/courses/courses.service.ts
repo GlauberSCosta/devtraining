1defd48ef988e30c29a7cd6a29a7a77cbb9db8e9
Gláuber da Silva Costa <glaubscosta@gmail.com>
2023-02-07 23:43:52 -0300

    typeorm entity relationship

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -7,34 +7,55 @@ import {
 import { Course } from './entities/course.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Tag } from './entities/tag.entity/tag.entity';
+import { CreateCourseDto } from './dto/create-course.dto';
 
 @Injectable()
 export class CoursesService {
   constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+    @InjectRepository(Course)
+    private readonly tagRepository: Repository<Tag>,
   ) {}
 
   findAll() {
-    return this.courseRepository.find();
+    return this.courseRepository.find({
+      relations: ['tags'],
+    });
   }
   findOne(id: string) {
-    const course = this.courseRepository.findOneById(id);
+    const course = this.courseRepository.findOne({
+      where: {
+        id: +id,
+      },
+      relations: ['tags'],
+    });
     if (!course) {
       throw new NotFoundException(`Course ID ${id} not found`);
     }
     return course;
   }
 
-  create(createCourseDto: any) {
-    const course = this.courseRepository.create(createCourseDto);
+  async create(createCourseDto: CreateCourseDto) {
+    const tags = await Promise.all(
+      createCourseDto.tags.map((name: string) => this.preloadTagByName(name)),
+    );
+    const course = this.courseRepository.create({ ...createCourseDto, tags });
     return this.courseRepository.save(course);
   }
 
   async update(id: string, updateCourseDto: any) {
+    const tags =
+      updateCourseDto.tags &&
+      (await Promise.all(
+        updateCourseDto.tags.map((name) => this.preloadTagByName(name)),
+      ));
+
     const course = await this.courseRepository.preload({
       id: +id,
       ...updateCourseDto,
+      tags,
     });
     if (!course) {
       throw new NotFoundException(`Course ID ${id} not found`);
@@ -48,5 +69,12 @@ export class CoursesService {
       throw new NotFoundException(`Course ID ${id} not found`);
     }
     return this.courseRepository.remove(course);
+  }
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOneBy({ name });
+    if (tag) {
+      return tag;
+    }
+    return this.tagRepository.create({ name });
   }
 }
--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Tag } from './tag.entity/tag.entity';
 
 @Entity('courses')
 export class Course {
@@ -8,6 +15,9 @@ export class Course {
   name: string;
   @Column()
   description: string;
-  @Column('json', { nullable: true })
-  tags: string[];
+  @JoinTable()
+  @ManyToMany(() => Tag, (tag: Tag) => tag.courses, {
+    cascade: true,
+  })
+  tags: Tag[];
 }
